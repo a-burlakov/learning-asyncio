a08d0f86a6c63a6fa9464a5a25fd5d2870f7ad1f
a-burlakov <fln@mail.ru>
2023-04-04 11:52:15 +0500

    lesson: plain code.

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import asyncio
+from time import sleep
+
+
+def make_request(cnt):
+    print("Запрос к БД:", cnt)
+    sleep(0.1)
+
 
 if __name__ == "__main__":
-    print("let's go!")
+
+    cnt = 0
+    for _ in range(100):
+        make_request(cnt)
+        cnt += 1
