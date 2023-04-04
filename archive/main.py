392f22da01ee72628685b90ff0e286c8bb38ba1a
a-burlakov <fln@mail.ru>
2023-04-04 14:46:30 +0500

    lesson: some code.

---
+++ archive/main.py
@@ -0,0 +1,29 @@
+import asyncio
+from asyncio import sleep
+
+QUERY = "INSERT INTO some_test_table VALUES ($1, $2, $3)"
+
+
+async def make_request():
+    await sleep(0.1)
+
+
+async def main():
+    chunk = 200
+    tasks = []
+    pended = 0
+    for x in range(1000):
+        task = asyncio.create_task(make_request())
+        pended += 1
+        tasks.append(task)
+
+        if len(tasks) == 200 or pended == 10_000:
+
+            res = await asyncio.gather(*tasks)
+            tasks.clear()
+            print(pended)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
--- main.py
+++ main.py
@@ -1,29 +1,24 @@
 import asyncio
+import threading
 from asyncio import sleep
 
-QUERY = "INSERT INTO some_test_table VALUES ($1, $2, $3)"
+
+def multiply(a, b):
+    return a * b
 
 
-async def make_request():
-    await sleep(0.1)
+def generator(a, b):
+    while True:
+        yield a * b
+        a = a + 1
 
 
-async def main():
-    chunk = 200
-    tasks = []
-    pended = 0
-    for x in range(1000):
-        task = asyncio.create_task(make_request())
-        pended += 1
-        tasks.append(task)
-
-        if len(tasks) == 200 or pended == 10_000:
-
-            res = await asyncio.gather(*tasks)
-            tasks.clear()
-            print(pended)
+async def async_function(a):
+    while True:
+        await a
+        a = a + 1
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    a = async_function(2)
+    print(dir(a))
